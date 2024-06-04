195c36ef8e29337d8e569a9004240a03441465ea
Arun Daniel <arudani@cdw.com>
2024-06-04 19:09:11 -0400

    Merge remote-tracking branch 'origin/master' into master

--- all.tf
+++ all.tf
@@ -8,9 +8,7 @@ terraform {
 }
 #aws stuff
 provider "aws" {
-    region = var.region
-    access_key = var.access_key
-    secret_key = var.secret_key
+    region = "us-east-1"
 }
 
 variable "access_key" {
