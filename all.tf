893c9c3668578f2b6c623cd096c7ac553df0ff82
Arun Daniel <arudani@cdw.com>
2024-06-04 19:03:30 -0400

    Merge remote-tracking branch 'origin/master' into master

---
+++ all.tf
@@ -0,0 +1,31 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+#aws stuff
+provider "aws" {
+    region = var.region
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+variable "access_key" {
+    type = string
+}
+
+variable "secret_key" {
+    type = string
+}
+
+variable "region" {
+    type = string
+    default = "<%=customOptions.awsRegion%>"
+}
+
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.10.0.0/16"
+}
