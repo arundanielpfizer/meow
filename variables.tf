7327364be52026613fedda77982a452f41283917
Arun Daniel <arudani@cdw.com>
2024-06-05 17:13:05 -0400

    Merge remote-tracking branch 'origin/master' into master

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
-  description = "The AWS region to deploy in"
+  description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"  # Replace with your desired region
 }
 
 variable "access_key" {
