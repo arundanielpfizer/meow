0643274bd2a8f81b3bcc73bd16d1cea6e72d8862
Arun Daniel <arudani@cdw.com>
2024-06-05 07:04:17 -0400

    Merge remote-tracking branch 'origin/master' into master

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-terraform {
+/* terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws"
@@ -6,8 +6,27 @@ terraform {
         }
     }
 }
+ */
+
+variable "region" {
+  description = "The AWS region to deploy in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "access_key" {
+  description = "The AWS access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The AWS secret key"
+  type        = string
+  sensitive   = true
+}
 
 provider "aws" {
+    region = var.region
   access_key = var.access_key
   secret_key = var.secret_key
 }
