59501926674a572f675dd79f3f5cdf5b46782571
Arun Daniel <arudani@cdw.com>
2024-06-05 17:16:29 -0400

    Merge remote-tracking branch 'origin/master' into master

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.52.0"
+    }
+  }
+}
+
+#aws stuff
+provider "aws" {
+    region = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-00beae93a2d981137"
   instance_type = "t2.micro"
